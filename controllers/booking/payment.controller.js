c17bdf04f0d82995534a05bd3f114d5ea201abc2
tuananh7-dev <tuananh7.dev@gmail.com>
2024-04-18 22:26:57 +0700

    feat: insert to show time when booking

--- controllers/booking/payment.controller.js
+++ controllers/booking/payment.controller.js
@@ -1,3 +1,4 @@
+const showTimeStorage = require("../../storages/show-time.storage");
 const { randomString } = require("../../helpers/random.helper");
 
 async function genQRCode(req, res) {
@@ -11,6 +12,7 @@ async function genQRCode(req, res) {
     if (!seatSelected || seatSelected.length === 0) {
         return res.json({ message: "Vui lòng chọn ghế" });
     }
+
     const amount =
         seatSelected.reduce((amount, seat) => {
             return (amount += seat.price);
@@ -34,9 +36,30 @@ async function genQRCode(req, res) {
     })
         .then((res) => res.json())
         .then((res) => res.data.qrDataURL);
+
+    // Them vao mang show time
+    const userId = req.payload.data.id;
+    const showTimeId = req.body.showTimeId;
+    const timeId = req.body.timeId;
+    const seatIds = seatSelected.map((seat) => seat.seatId);
+
+    const showTime = showTimeStorage.find((showTime) => showTime.id === showTimeId);
+    const time = showTime.infos.find((info) => info.id == timeId);
+    let bookedSeatId;
+    if (time.bookedSeats.length > 0) {
+        bookedSeatId = time.bookedSeats[time.bookedSeats.length - 1].id + 1;
+    } else {
+        bookedSeatId = 1;
+    }
+    time.bookedSeats.push({
+        id: bookedSeatId,
+        user_id: userId,
+        seats: seatIds,
+    });
     const data = [
         {
             content: addInfo,
+            showTime,
             qrCodeBase64: response,
         },
     ];
